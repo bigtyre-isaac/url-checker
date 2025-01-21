ebc110be07f9a9d9fed527b277b9b0943c4466e7
Isaac <isaac@bigtyre.com.au>
2025-01-21 10:06:18 +1000

    Added Prometheus metrics support.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
-# app.py
 from flask import Flask, render_template, jsonify
 import json
 import os
 import logging
 from filelock import FileLock
+from prometheus_client import make_wsgi_app, Gauge, CollectorRegistry
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 app = Flask(__name__)
-
+registry = CollectorRegistry(auto_describe=False)
+url_up = Gauge('url_response_success', 'URL Up/Down Status', ['url'], registry=registry)
 
 data_path = "./data/status.json"
-lock_path = "./data/status.json.lock"  # Lock file path
+lock_path = "./data/status.json.lock"
 
 # Configure logging
 log_level = os.getenv("LOG_LEVEL", "INFO").upper()
@@ -18,13 +20,16 @@ logger = logging.getLogger(__name__)
 
 logger.info("Application started.")
 base_path = os.getenv("BASE_PATH", "/")
-logger.info("Using base path: '{base_path}'")
+logger.info(f"Using base path: {base_path}")
 
 def load_status():
   with FileLock(lock_path):
     try:
       with open(data_path, "r") as f:
-        return json.load(f)
+        data = json.load(f)
+        for url, info in data.items():
+            url_up.labels(url).set(1 if info['status'] else 0)
+        return data
     except (FileNotFoundError, json.JSONDecodeError) as e:
       logger.warning(f"Could not load status data: {e}")
       return {}
@@ -38,6 +43,11 @@ def index():
 def status():
     status = load_status()
     return jsonify(status)
+
+# Add route for Prometheus metrics
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app(registry)
+})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
